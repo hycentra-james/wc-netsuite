3d51ee4cdfb50e4346db96a02a27f60f283fc006
James Lui <124945698+hycentra-james@users.noreply.github.com>
2025-09-04 15:15:41 -0700

    Updating FTP helper to support Netsuite OOTB secret

--- src/FileCabinet/SuiteScripts/Hycentra/Integrations/FTP_SFTP/ftpHelper.js
+++ src/FileCabinet/SuiteScripts/Hycentra/Integrations/FTP_SFTP/ftpHelper.js
@@ -23,6 +23,7 @@ define(['N/log', 'N/sftp'],
             var username = customerRecord.getValue({ fieldId: 'custentity_hyc_cust_ftp_username' });
             var password = customerRecord.getValue({ fieldId: 'custentity_hyc_cust_ftp_password' });
             var passwordGuid = customerRecord.getValue({ fieldId: 'custentity_hyc_cust_ftp_pwd_guid' });
+            var secret = customerRecord.getValue({ fieldId: 'custentity_hyc_cust_ftp_secret' });
             var hostKey = customerRecord.getValue({ fieldId: 'custentity_hyc_cust_ftp_host_key' });
             var privateKeyId = customerRecord.getValue({ fieldId: 'custentity_hyc_cust_ftp_pkeyid' });
             
@@ -32,6 +33,7 @@ define(['N/log', 'N/sftp'],
             log.debug('DEBUG', 'username = ' + username);
             log.debug('DEBUG', 'password = ' + password);
             log.debug('DEBUG', 'passwordGuid = ' + passwordGuid);
+            log.debug('DEBUG', 'secret = ' + secret);
             log.debug('DEBUG', 'hostKey = ' + hostKey);
             log.debug('DEBUG', 'privateKeyId = ' + privateKeyId);
 
@@ -54,6 +56,16 @@ define(['N/log', 'N/sftp'],
                     port: parseInt(port.trim()),
                     directory: '/', // Set default directory to root
                     hostKey: hostKey
+                });
+            } else if (secret && secret !== null && secret !== '') {
+                log.debug('DEBUG', 'Using secret');
+                connection = sftp.createConnection({
+                    username: username,
+                    secret: secret, // Use secret if you have secret instead of private key
+                    url: host,
+                    port: parseInt(port.trim()),
+                    directory: '/', // Set default directory to root
+                    hostKey: hostKey                    
                 });
             }
 
